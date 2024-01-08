d8629eefa62a118847497d84c325093f1b672f46
Gyutae Kim <kiimwf0819@gmail.com>
2024-01-08 17:24:56 +0900

    feat(MemberService): 서비스로직 클래스 작성

---
+++ src/main/java/study/member/service/MemberService.java
@@ -0,0 +1,51 @@
+package study.member.service;
+
+import java.util.List;
+import java.util.Optional;
+import study.member.domain.Member;
+import study.member.repository.MemberRepository;
+import study.member.repository.MemoryMemberRepository;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        /*
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+
+        Optional 사용 시 아래와 같이 줄여서 사용 가능함
+        */
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
