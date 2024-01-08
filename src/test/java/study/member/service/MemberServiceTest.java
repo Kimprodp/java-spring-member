729f59253e00021b11a7adf430c80d49af0c8488
Gyutae Kim <kiimwf0819@gmail.com>
2024-01-08 17:25:21 +0900

    Test(MemberServiceTest): 테스트 코드 작성

---
+++ src/test/java/study/member/service/MemberServiceTest.java
@@ -0,0 +1,87 @@
+package study.member.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import study.member.domain.Member;
+import study.member.repository.MemoryMemberRepository;
+
+class MemberServiceTest {
+
+    MemoryMemberRepository memberRepository;
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    public void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("Kim");
+
+        //when
+        Long saveId = memberService.join(member);
+        Member findMember = memberService.findOne(saveId).get();
+
+        //then
+        assertThat(findMember.getName()).isEqualTo(member.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("Kim");
+
+        Member member2 = new Member();
+        member2.setName("Kim");
+
+        //when
+        memberService.join(member1);
+
+        //then
+        assertThatThrownBy(() -> memberService.join(member2))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("이미 존재하는 회원입니다.");
+
+        /*
+        <또 다른 방법 1 - 권장되지 않음>
+        try {
+            memberService.join(member2);
+            fail();
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        }
+
+        */
+
+        /*
+        <또 다른 방법 2>
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+         */
+    }
+
+    @Test
+    public void confirmFindMembers() {
+
+    }
+
+    @Test
+    public void confirmFindOne() {
+
+    }
+
+}
