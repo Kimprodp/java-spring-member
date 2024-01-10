211f35f6c6e79ab730574fe547427e739ba1aabc
Gyutae Kim <kiimwf0819@gmail.com>
2024-01-10 18:36:39 +0900

    feat(MemberContorller): 컨트롤러 추가

--- src/main/java/study/member/controller/MemberController.java
+++ src/main/java/study/member/controller/MemberController.java
@@ -1,7 +1,12 @@
 package study.member.controller;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import study.member.domain.Member;
 import study.member.service.MemberService;
 
 @Controller
@@ -12,5 +17,27 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createFrom() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
